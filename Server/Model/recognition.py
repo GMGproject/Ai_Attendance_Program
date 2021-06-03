119979e450f4cdfa2116c12dd77d68f61c1c3000
v4chelsea <56423426+v4chelsea@users.noreply.github.com>
2021-06-03 22:06:27 +0900

    move dir './Model' to './Server/Model'

---
+++ Server/Model/recognition.py
@@ -0,0 +1,22 @@
+# Face Recognition
+
+import face_recognition
+import cv2
+import numpy as np
+
+def getFaceEncoding(faceDir, name):
+    '''
+    arg discription
+    faceDir : face image dir
+    name : person's name in face image
+
+    return
+    faceDictData : name + face encoding dictionary data
+    '''
+
+    image = face_recognition.load_image_file(faceDir)
+    faceEncoding = face_recognition.face_encodings(image)[0]
+    faceDictData = {name : faceEncoding}
+
+    return faceDictData
+
---
+++ Server/Model/utils.py
@@ -0,0 +1,44 @@
+'''
+def drawRectangle():
+    text = "{:.2f}%".format(confidence * 100)
+    y = startY - 10 if startY - 10 > 10 else startY + 10 # set y position to write percentage of confidence value on left top of boxes
+    cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+'''
+
+def checkFaceSize(faces, size=100):
+    '''
+    args discription
+    faces : detected faces list from frame
+    size : to set standard of face size
+     - default = 100
+
+    return
+    passCheck : variables to determine the next process
+    passFaces : faces that satisfied our own standard
+    '''
+
+    passedFaces = []
+    passCheck = False
+
+    if len(faces) == 0:
+        pass
+    else:    
+        for face in faces:
+            if (face[2] - face[0]) > size:
+                passedFaces.append(face)
+                passCheck = True
+
+    return passCheck, passedFaces
+
+def chanegeAttendance(resultList):
+    '''
+    args discription
+    resultList : List of recognized names
+    '''
+    resultName = max(resultList, key=resultList.count)
+    
+    if resultName == "Unknown":
+        print("처음뵙는분이군요 반갑습니다.")
+    else:        
+        print("{}님 출석입니다.".format(resultName))
