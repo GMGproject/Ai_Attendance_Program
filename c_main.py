941e5cd2a028ef17827cac9a0e4c6b821a1bed91
v4chelsea <56423426+v4chelsea@users.noreply.github.com>
2021-06-03 22:08:32 +0900

    move each Folders main.py file

---
+++ c_main.py
@@ -0,0 +1,32 @@
+import socket, argparse
+from Client.client import connectWithServer, disconnectWithServer
+
+def main(args):
+    host = args.host
+    port = args.port
+    cam = args.cam
+
+    # create client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    connectWithServer(client_socket=client_socket,
+                        host=host, 
+                        port=port)
+    while True:
+        print("helloworld")
+        #break
+        
+    disconnectWithServer(client_socket)
+    
+
+if __name__ == "__main__":
+    # create parser instance
+    parser = argparse.ArgumentParser(description='Client Args ip, port, camIndex')
+
+    # Setup input values
+    parser.add_argument('--host', type=str, default="localhost")
+    parser.add_argument('--port', type=int, default=9999)
+    parser.add_argument('--cam',  type=int, default=0)
+
+    args = parser.parse_args()
+    main(args)
---
+++ s_main.py
@@ -0,0 +1,42 @@
+import argparse
+import threading
+
+from Server.server import createServerSocket, returnInternalHost
+from Server.server import recvData
+
+from Server.Model.model import train
+
+print("Training KNN classifier...")
+classifier = train("./Server/Model/train", model_save_path="./Server/Model/weights/trained_knn_model.clf", n_neighbors=2)
+print("Training complete!")
+
+def main(args):
+
+    host = args.host
+    port = args.port
+    client_socket = None
+
+    # create server & client socket
+    server_socket = createServerSocket(host=host, port=port)
+    client_socket = None
+
+    while True:
+        print("waiting client")
+        client_socket, addr = server_socket.accept()
+        if client_socket != None:
+            print("Connect with {0}:{1}".format(client_socket, addr))
+            recvData(client_socket)
+            
+if __name__ == "__main__":
+    # get internal host
+    internalHost = returnInternalHost()
+
+    # create parser instance
+    parser = argparse.ArgumentParser(description='Server Args ip, port')
+
+    # Setup input values
+    parser.add_argument('--host', type=str, default=internalHost)
+    parser.add_argument('--port', type=int, default=9999)
+
+    args = parser.parse_args()
+    main(args)
