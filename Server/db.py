a8bb0a649172c139ad650e0aadb89bca06320cdd
v4chelsea <56423426+v4chelsea@users.noreply.github.com>
2021-06-04 17:11:15 +0900

    add functions which related with DB control

---
+++ Server/db.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+# Connect to MySQL DB
+def connectDB():
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="1234",
+            database="AttendanceProgram")
+
+        return db
+    except Exception as e:
+        print("Connection Error : " + e)
+
+# Disconnect with connected DB
+def disconnectDB(db):
+    db.close()
+
+# query for Insert, Update, Delete
+def queryIUD(cursor, sql):
+    try:
+        cursor.execute(sql)
+    except Exception as e:
+        print(e)
+
+# query for Select & print Selected Data
+def queryS(cursor, sql):
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for column in result:
+            print(column)
+    except Exception as e:
+        print(e)
+
+# Parse sql to check type & help to do their each part 
+def queryExecutor(sql):
+    db = connectDB()
+    cursor = db.cursor()
+    type = sql.split(' ')[0]
+
+    if type.upper() == 'SELECT':
+        queryS(cursor, sql)
+    else:
+        queryIUD(cursor, sql)
+        db.commit()
+
+    disconnectDB(db)
