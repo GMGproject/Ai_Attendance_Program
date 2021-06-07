a7815365ba8a87a39d3dc29c12d989c9a1ba24e6
v4chelsea <56423426+v4chelsea@users.noreply.github.com>
2021-06-08 04:30:59 +0900

    Final commit

--- c_main.py
+++ c_main.py
@@ -1,24 +1,15 @@
-import socket, argparse
+import socket
 import sys
 
 from Client.ui import WindowClass
 
 from PyQt5.QtWidgets import *
 
-def main(args):
+def main():
     app = QApplication(sys.argv)
     mainWindow = WindowClass()
     mainWindow.show()
     app.exec_()            
 
 if __name__ == "__main__":
-    # create parser instance
-    parser = argparse.ArgumentParser(description='Client Args ip, port, camIndex')
-
-    # Setup input values
-    parser.add_argument('--host', type=str, default="localhost")
-    parser.add_argument('--port', type=int, default=9999)
-    parser.add_argument('--cam',  type=int, default=0)
-
-    args = parser.parse_args()
-    main(args)
+    main()
